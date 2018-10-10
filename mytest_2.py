40cf898e4842d5d3ee03a67dc5746a1d33ba0c52
unknown <diopwas@yahoo.fr>
2018-10-10 16:49:31 +0200

    troisieme modif

--- mytest_2.py
+++ mytest_2.py
@@ -3,3 +3,5 @@ from score_personne import *  # j'importe le module contenant ma fonction
 class MyFirstTests(unittest.TestCase):
     def test_person1(self):
         self.assertEqual(score_person('Joseph', 15), '66%')
+
+        # je viens de faire un 3e commit
